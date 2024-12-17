e9d991736e6b674cdaca29e775d22f808d51e503
chrisdev-ui <web.christian.dev@gmail.com>
2024-12-17 18:45:11 -0500

    feat: update next config to disable suspense required

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
+        missingSuspenseWithCSRBailout: false,
         serverActions: {
             allowedOrigins: ["localhost:3000","c5qxs3h3-3000.use2.devtunnels.ms", "*.c5qxs3h3-3000.use2.devtunnels.ms"]
         }
